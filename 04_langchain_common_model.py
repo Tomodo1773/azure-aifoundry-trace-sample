ce491b8beb3f8ea1c23c38620be0a945673c79e2
Tomodo1773 <forever-career@hotmail.co.jp>
2025-04-27 21:34:00 +0900

    Azure OpenAIとLangChainの統合を実装し、環境変数からの設定を追加

---
+++ 04_langchain_common_model.py
@@ -0,0 +1,56 @@
+import os
+
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import AzureChatOpenAI
+from openinference.instrumentation.langchain import LangChainInstrumentor
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
+
+load_dotenv()
+
+project_client = AIProjectClient.from_connection_string(
+    credential=DefaultAzureCredential(),
+    conn_str=os.environ["PROJECT_CONNECTION_STRING"],
+)
+
+application_insights_connection_string = project_client.telemetry.get_connection_string()
+
+exporter = AzureMonitorTraceExporter.from_connection_string(application_insights_connection_string)
+
+tracer_provider = TracerProvider()
+trace.set_tracer_provider(tracer_provider)
+
+span_processor = BatchSpanProcessor(exporter, schedule_delay_millis=60000)
+tracer_provider.add_span_processor(span_processor)
+tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+
+tracer = trace.get_tracer(__name__)
+
+LangChainInstrumentor().instrument()
+
+prompt = ChatPromptTemplate.from_messages([("system", "You are a helpful assistant."), ("user", "{text}")])
+
+parser = StrOutputParser()
+
+
+model = AzureChatOpenAI(
+    api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_version="2025-01-01-preview",
+    azure_deployment="gpt-4o",
+)
+
+chain = prompt | model | parser
+chain_config = chain.with_config(run_name="Langchain_AzureOpenAITracer")
+response = chain_config.invoke({"text": "What is the capital of France?"})
+# response = chain.invoke({"text": "What is the capital of France?"})
+
+print("***Response from the model***")
+print(response)
+print("***End of response from the model***")
